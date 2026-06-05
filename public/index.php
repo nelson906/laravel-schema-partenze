277987e35ea52875411b4c3f68ec65b0528d9326
nelson906 <am.grippa@tiscali.it>
2026-06-05 14:06:35 +0200

    migliorie

--- public/index.php
+++ public/index.php
@@ -5,6 +5,10 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Subdirectory fix: facciamo credere a Symfony che lo script sia a /partenze/index.php
+$_SERVER['SCRIPT_NAME'] = '/partenze/index.php';
+$_SERVER['PHP_SELF'] = '/partenze/index.php';
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
